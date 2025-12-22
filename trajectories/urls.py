0ce5656eea7e45720ef79ea83549dcfa4fca9490
MichalLopaciuch <michal.lopaciuch@gmail.com>
2025-12-22 18:49:00 +0100

    Implement TSP Solution

--- trajectories/urls.py
+++ trajectories/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import find_trajectory
+from .views import find_trajectory, visit_trajectories
 
 
-urlpatterns = [path("find", find_trajectory)]
+urlpatterns = [
+    path("find", find_trajectory),
+    path("visit", visit_trajectories),
+]
